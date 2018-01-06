9f49fcdcefcc4b61a97523a25abce3a5f7087d8f
DESKTOP-3HJ3ESF\SHAURYA <rathodsurendra18@gmail.com>
2018-01-06 18:39:14 +0530

    Add quote layout & index pages

---
+++ InsurancePortal.Web/Models/ModelQuestions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsurancePortal.Web.Models
+{
+    public class ModelQuestions
+    {
+        public List<ProgressBar> ProgressBarsList { get; set; }
+
+        public List<Sections> SectionsList { get; set; }
+    }
+
+    public class ProgressBar
+    {
+        public Guid ProgressBarId { get; set; }
+
+        public string ProgressBarTitle { get; set; }
+    }
+
+    public class Sections
+    {
+        public Guid SectionId { get; set; }
+
+        public Guid ProgressBarId { get; set; }
+
+        public string SectionTitle { get; set; }
+
+        public List<Questions> QuestionsList { get; set; }
+    }
+
+    public class Questions
+    {
+        public Guid QuestionId { get; set; }
+
+        public string QuestionTitle { get; set; }
+
+        public int QuestionType { get; set; }
+
+        public string SubmittedAnswer { get; set; }
+
+        public string ValidationMessage { get; set; }
+
+        public List<Answers> AnswersList { get; set; }
+    }
+
+    public class Answers
+    {
+        public Guid AnswerId { get; set; }
+
+        public string AnswerTitle { get; set; }
+    }
+}
